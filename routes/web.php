8c68c1ca5f50dfa820c565281fc7f8ee6ca2e10c
Elizabeth Gita <elizabeth.gita.tif423@polban.ac.id>
2025-05-22 23:28:32 +0700

    UI slicing basic technical half progress

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\Home\HomeController;
 use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\ScheduleController;
 
 
 
@@ -13,8 +16,49 @@ Route::middleware('auth')->group(function() {
     // Routes - Home
     Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-    // Routes - Proyek
+    // Route for dashboard page
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    
+    // Project Routes with better naming convention
+    Route::prefix('projects')->group(function() {
+        // List all projects
+        Route::get('/', [ProjectController::class, 'index'])->name('projects.index');
+        
+        // Create new project form
+        Route::get('/create', [ProjectController::class, 'create'])->name('projects.create');
+        
+        // Add project form
+        Route::get('/add', [ProjectController::class, 'add'])->name('projects.add');
+        
+        // Store new project
+        Route::post('/', [ProjectController::class, 'store'])->name('projects.store');
+        
+        // Show project details (must be after /create to prevent conflicts)
+        Route::get('/{id}', [ProjectController::class, 'show'])->name('projects.show');
+        
+        // Edit project form
+        Route::get('/{id}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
+        
+        // Update project
+        Route::put('/{id}', [ProjectController::class, 'update'])->name('projects.update');
+        
+        // Delete project
+        Route::delete('/{id}', [ProjectController::class, 'destroy'])->name('projects.destroy');
+    });
+    
+    // Schedule Routes
+    Route::prefix('schedule')->group(function() {
+        // View schedule dashboard
+        Route::get('/', [ScheduleController::class, 'index'])->name('schedule.index');
+        
+        // Edit schedule page - rename this line if you want
+        Route::get('/schedule', [ScheduleController::class, 'schedule'])->name('schedule.edit');
+        
+        // Update schedule
+        Route::post('/update', [ScheduleController::class, 'update'])->name('schedule.update');
+    });
 
+    // Add other authenticated routes here
 });
 
 Route::middleware('guest')->group(function() {
