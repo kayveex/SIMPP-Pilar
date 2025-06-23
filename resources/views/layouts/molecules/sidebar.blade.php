798f4c49a9e9979f2266752d673a0a386f6c0b15
unknown <rhesakornelius@gmail.com>
2025-06-23 19:50:49 +0700

    fix: final revision before UAT #2

--- resources/views/layouts/molecules/sidebar.blade.php
+++ resources/views/layouts/molecules/sidebar.blade.php
@@ -68,7 +68,7 @@
             <div class="mt-4 flex-col">
                 <label for="arsip" class="font-bold">SUPER ADMIN</label>
                 <a href="{{ route('user.index') }}"
-                    class="flex flex-row items-center text-lg font-semibold mt-2 gap-2 {{ request()->is('arsip-proyek*') ? 'text-[#3D42DF]' : 'hover:text-[#3D42DF]' }}">
+                    class="flex flex-row items-center text-lg font-semibold mt-2 gap-2 {{ request()->is('user*') ? 'text-[#3D42DF]' : 'hover:text-[#3D42DF]' }}">
                     <i class="ph-bold ph-users-three"></i>
                     <span>Akun</span>
                 </a>
