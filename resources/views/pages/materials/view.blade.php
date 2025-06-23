533fa47b6c85078e613b89cdc07e46088388872e
unknown <rhesakornelius@gmail.com>
2025-06-23 18:50:41 +0700

    fix: features on search material & persetujuan #1

--- resources/views/pages/materials/view.blade.php
+++ resources/views/pages/materials/view.blade.php
@@ -233,7 +233,7 @@
                         <i class="ph-bold ph-list-checks"></i>
                         <h2 class="text-lg font-bold">Persetujuan Material</h2>
                     </div>
-                    @if ($material->purchasing_approval === false)
+                    @if ($material->purchasing_approval == false || $material->purchasing_approval == 0)
                         <div class="flex flex-col border-2 border-blue-600 rounded-bl-lg rounded-tr-lg rounded-br-lg p-4">
                             <h3 class="text-lg font-semibold">
                                 Material <span class="font-bold">{{ $material->material_title }}</span> dari proyek <span class="font-bold">{{ $material->project->project_name }} </span> belum disetujui oleh Divisi Purchasing.
@@ -241,7 +241,7 @@
                             <p class="mt-2 text-gray-600">Silakan tunggu persetujuan dari Divisi Purchasing.</p>
                         </div>
 
-                    @elseif ($material->purchasing_approval === true)
+                    @elseif ($material->purchasing_approval == true || $material->purchasing_approval == 1)
                         <div class="flex flex-col border-2 border-blue-600 rounded-bl-lg rounded-tr-lg rounded-br-lg p-4">
                             <h3 class="text-lg font-semibold">
                                 Material <span class="font-bold">{{ $material->material_title }}</span> dari proyek <span class="font-bold">{{ $material->project->project_name }} </span> telah disetujui oleh Divisi Purchasing.
